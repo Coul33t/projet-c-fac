2fd6eef52cc0e8db8a0f4575d10b93f6e93c707f
Quentin Couland <Coulis1990@gmail.com>
2025-11-01 18:06:42 +0100

    :memo: Added comments for helper functions

--- fonctions_aide.h
+++ fonctions_aide.h
@@ -21,17 +21,34 @@ enum Plan {
     ARRIERE, AVANT
 };
 
+// Initialise la console de Curses
 bool initialiser_console();
+
+// Efface la console
 void effacer_console();
+
+// Ferme la console de Curses
 void fermer_console();
+
+// Fait en sorte que l'appel à getch() soit non-bloquant
 void entree_non_bloquante();
+
+// Fait en sorte que l'appel à getch() soit bloquant
 void entre_bloquante();
 
+// Ecrit un caractère chr à l'emplacement (x, y)
 void ecrire_char(int x, int y, char chr);
+
+// Bouge le curseur console à l'emplacement (x, y)
 void bouger_curseur(int x, int y);
 
+// Remet les couleurs par défaut pour les affichages (blanc sur noir)
 void couleur_defaut();
+
+// Choisi une couleur pour les affichages
 void changer_couleur(Couleur couleur, Plan plan);
+
+// Ecrit une chaine de caractère à partir de l'emplacement (x, y)
 void ecrire_string(const std::string& message, int x, int y);
 
 #endif
